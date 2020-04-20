4e51a7f5685c884838c985ddb1b63473075c94d1
Josh Beglan <josh_beglan@yahoo.com>
2020-04-20 23:52:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,67 @@
 # Write your code here!
+
+def game_hash = {
+  :home => {
+    :team_name => ,
+    :colors => [],
+    :players => [
+      
+      ]
+  },
+  :away => {
+    :team_name => ,
+    :colors => [],
+    :players => [
+      { :player_name => ,
+        :number => ,
+        :shoe => ,
+        :points =>
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks => ,
+      },
+      { :player_name => ,
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks => ,
+      },
+      { :player_name => ,
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks => ,
+      },
+      { :player_name => ,
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks => ,
+      },
+      { :player_name => ,
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks => ,
+      }
+    ]
+  }
+}
